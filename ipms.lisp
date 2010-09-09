253ac43a61d853cded6ab4c8a4775fa349dca689
martin <martin@cyberpower.kcl.ac.uk>
2010-09-09 10:51:35 +0100

    external trigger switch, there was a bug in handling return value of status

--- ipms.lisp
+++ ipms.lisp
@@ -4,6 +4,13 @@
   (:use :cl :ipms-ffi))
 (in-package :ipms)
  
+(defun set-extern-trigger (&optional (on t))
+  (if on
+      (unless (= 0 (enable-extern-start))
+	(error "enable-extern-start didn't return 0."))
+      (unless (= 0 (disable-extern-start))
+	(error "disable-extern-start didn't return 0."))))
+
 (defun init ()
   (register-board #x0036344B00800803
 		  "192.168.0.2"
@@ -19,16 +26,13 @@
 
 (defun write-data (buf &key (pic-number 1))
   "Write a 256x256 unsigned-short buffer to the device."
-  (declare ((simple-array (unsigned-byte 16) 2) buf)
+  (declare ((simple-array (unsigned-byte 16) (256 256)) buf)
 	   (values null &optional))
-  (sb-sys:with-pinned-objects (buf)
-    (write-matrix-data pic-number 3
-		       (sb-sys:vector-sap 
-			(sb-ext:array-storage-vector 
-			 buf))
-		       (* 256 256)))
+  (let ((buf1 (sb-ext:array-storage-vector buf)))
+    (sb-sys:with-pinned-objects (buf)
+      (write-matrix-data pic-number 3 (sb-sys:vector-sap buf1) (length buf1))))
   nil)
- 
+
 (defun draw (&key (r-small 0.0) (r-big 1.0) (pic-number 1))
   (declare (single-float r-small r-big)
 	   (values null &optional))
@@ -36,7 +40,7 @@
 	 (nh (floor n 2))
 	 (1/n (/ 1.0 n))
 	 (buf (make-array (list n n) 
-			:element-type '(unsigned-byte 16))))
+			  :element-type '(unsigned-byte 16))))
     (declare (type (simple-array (unsigned-byte 16) (* *))))
     (dotimes (j n)
       (dotimes (i n)
@@ -44,72 +48,44 @@
 	       (y (* 2.0 1/n (- j nh)))
 	       (r (sqrt (+ (* x x) (* y y)))))
 	  (setf (aref buf i j) 
-		(if (< r-small r r-big)
-		    0
-		    #xffff)))))
+		(if (< r-small r r-big) 0 #xffff)))))
     (write-data buf :pic-number pic-number)
     nil))
- 
+
 (defun parse-bits (value bits)
+  (declare (fixnum value))
   (let ((result nil))
     (loop for (name x) in bits do
 	 (when (logand value x)
 	   (push name result)))
     result))
-
 (defun parse-status-bits (value)
-  (parse-bits value
-	      '((peltier-on #x1000)
-		(power-on #x4000)
-		(start-matrix #x8000)
-		(smart-adr-on #x10000)
-		(extern-start-en #x40000))))
-
-#+nil
-(parse-status-bits 392342) 
-
+  (parse-bits 
+   value '((peltier-on #x1000) (power-on #x4000) (start-matrix #x8000)
+	   (smart-adr-on #x10000) (extern-start-en #x40000))))
 (defun parse-error-bits (value)
   (parse-bits value
-	      '((mirror-voltage #x01)
-		(module-error #x02)
-		(calibration-error #x04)
-		(temperature-alert #x10)
-		(matrix-ready-error #x20)
-		(channel-overflow #x40)
-		(ram-test-error #x100)
-		(supply-error #x200)
-		(config-error #x400))))
-#+nil
-(parse-error-bits 99939)
-
+	      '((mirror-voltage #x01) (module-error #x02) (calibration-error #x04)
+		(temperature-alert #x10) (matrix-ready-error #x20) (channel-overflow #x40)
+		(ram-test-error #x100) (supply-error #x200) (config-error #x400))))
 (defun status ()
-  (multiple-value-bind (retval status error)
-      (read-status)
+  (multiple-value-bind (retval status error) (read-status)
     (unless (= 0 retval)
       (format t "read-status didn't return 0."))
-    (let ((lerror (parse-error-bits error)))
-     (if lerror
-       (format t "error(s) ~a detected, status: ~a, retval: ~a~%"
-	       (list error lerror) (parse-status-bits status) retval)
-       (format t "status ~a~%" (parse-status-bits status))))))
- 
-#+nil
+    (if (not (= 0 error))
+	(format t "error(s) ~a detected, status: ~a, retval: ~a~%"
+		(list error (parse-error-bits error)) (parse-status-bits status) retval)
+	(format t "status ~a~%" (parse-status-bits status)))))
+#+nil 
 (status)
-
-(defun update-picture (&key (r .5) (dr (- 1.0 r)))
-  (draw :r-small (- r dr) :r-big (+ r dr))
-  (set-picture-sequence 1 1 0))
- 
 (defun begin ()
   (set-power-on)
   (set-start-mma)
   (sleep 1) 
   (status))
- 
 (defun end ()
   (set-stop-mma)
   (set-power-off))
-
 (defun load-pictures (&key (n 12) (dr .02) (ready-out-needed 0))
   (dotimes (i n)
     (let ((r (/ .2 n)))
@@ -118,27 +94,22 @@
 	    :r-small (- r dr)
 	    :r-big (+ r dr))))
   (dotimes (i n)
-    (set-picture-sequence (1+ i) 
-			  (if (< i (- n 1))
-			      0
-			      1)
+    (set-picture-sequence (1+ i) (if (< i (- n 1)) 0 1)
 			  ready-out-needed)))
-
-#+nil
+#+nil 
 (time
  (progn
    (init)
    (load-pictures :n 4)
    (begin)))
-
-#+nil
+#+nil 
 (time
  (progn
    (set-stop-mma)
-   (load-pictures :n 3)
+   ;; (set-extern-trigger nil)
+   (load-pictures :n 2)
    (begin)))
-
-#+nil
+#+nil 
 (progn
   (end)
   (disconnect))
