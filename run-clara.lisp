38b65ed60e66cd0242ea4acaf4c4acf8c9e0a28a
martin <martin@hop.kcl.ac.uk>
2011-05-17 17:38:13 +0100

    sif-read single-float export doesn't work

--- run-clara.lisp
+++ run-clara.lisp
@@ -33,6 +33,7 @@
 (set-image 1 1
 	   1 1392
 	   1 1040)
+(set-ad-channel 1)
 
 (start-acquisition)
 (abort-acquisition)
@@ -65,4 +66,38 @@
 #+nil
 (save-camera-specs "/home/martin/0517/clara-e.cap")
 
+(let ((s *bl*))
+  
+  (subseq s 0 
+	  (position #\Space
+		    s)))
 
+
+
+(require :cl-utilities) ;; I need this to split a string into tokens
+
+(defun read-sif (fn)
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (with-open-file (s fn :direction :input)
+    (let ((head-lines
+	   (loop for i below 32 collect 
+		(read-line s))))
+      (destructuring-bind (a0 a1 h w a4 a5 a6 a7)
+	  (cl-utilities:split-sequence #\Space (elt head-lines 29))
+	(declare (ignore a0 a1 a4 a5 a6 a7))
+	(let* ((img (make-array (list (read-from-string h)
+				      (read-from-string w))
+				:element-type 'single-float))
+	       (img1 (sb-ext:array-storage-vector img))
+	       (pos (file-position s)))
+	  (declare (type (simple-array (unsigned-byte 32) 2) img))
+	  (with-open-file (s fn :element-type '(unsigned-byte 32))
+	    (file-position s pos)
+	    (read-sequence img1 s))
+	  img))))))
+
+
+(defparameter *bla*
+ (read-sif "/dev/shm/o.sif"))
+
+(uninit)
