66183387b89f3534e8b6927861bce298d8db7c8b
martin <martin@cyberpower.kcl.ac.uk>
2011-06-01 12:31:13 +0100

    binding for my libmma wrapper library to libIPMS

---
+++ libmma.lisp
@@ -0,0 +1,33 @@
+(defpackage :libmma
+  (:use :cl :sb-alien :sb-c-call))
+(in-package :libmma)
+(defparameter *library* "/home/martin/0505/mma-c/libmma.so") 
+(load-shared-object *library*)
+
+(define-alien-routine "upload_image" 
+    int
+  (buf (* unsigned-char)))
+ 
+(define-alien-routine "set_cycle_time"
+    int
+  (time-ms float))
+
+(define-alien-routine "init"
+    int)
+
+(define-alien-routine "uninit"
+    int)
+
+(define-alien-routine "status"
+    int
+  (stat unsigned-int :out)
+  (err unsigned-int :out))
+
+#+nil
+(init)
+#+nil
+(set-cycle-time 150s0)
+#+nil
+(uninit)
+#+nil
+(status)
