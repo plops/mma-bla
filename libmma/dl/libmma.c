e0964fa15149a11076925c85f20f3a0bb32cc3ad
martin <martin@cyberpower.kcl.ac.uk>
2011-06-02 15:36:06 +0100

    still doesnt work

--- libmma/dl/libmma.c
+++ libmma/dl/libmma.c
@@ -225,3 +225,46 @@ mma_uninit()
     e("disconnect");
   return 0;
 }
+
+enum{NN=N*N};
+
+unsigned short*
+splat(int i,int j, int d,unsigned short*buf)
+{
+  int x,y;
+  //for(x=0;x<NN;x++)
+  //  buf[x]=90;
+  for(y=-d;y<=d;y++)
+    for(x=-d;x<=d;x++){
+      int xx=x+i,yy=y+j;
+      if((0<=xx) && (xx<N) && (0<=yy) && (yy<N))
+      	buf[xx+N*yy]=4095;
+    }
+  return buf;
+}
+
+int
+all()
+{
+
+  int i,j;
+  unsigned short*buf=malloc(NN*sizeof(*buf));
+  splat(5,0,12,buf);
+  
+  if(0!=mma_init())
+    return -1;
+
+  for(i=0;i<6;i+=4)
+    for(j=0;j<6;j+=4){
+      printf("%d %d\n",i,j);
+      splat(i,j,7,buf);
+      printf("bla\n");
+      if(0!=mma_upload_image(buf)){
+	printf("error upload-image\n");
+      }
+      printf("blub\n");
+    }
+  if(0!=mma_uninit())
+    return -1;
+  return 0;
+}
--- libmma/dl/mma.c
+++ libmma/dl/mma.c
@@ -8,42 +8,44 @@ enum{N=256,NN=N*N};
 
 // echo "echo 4 > /proc/sys/net/ipv4/tcp_fin_timeout"  | sudo sh
 
-unsigned short*
-splat(int i,int j, int d,unsigned short*buf)
-{
-  int x,y;
-  //for(x=0;x<NN;x++)
-  //  buf[x]=90;
-  for(y=-d;y<=d;y++)
-    for(x=-d;x<=d;x++){
-      int xx=x+i,yy=y+j;
-      if((0<=xx) && (xx<N) && (0<=yy) && (yy<N))
-      	buf[xx+N*yy]=4095;
-    }
-  return buf;
-}
+/* unsigned short* */
+/* splat(int i,int j, int d,unsigned short*buf) */
+/* { */
+/*   int x,y; */
+/*   //for(x=0;x<NN;x++) */
+/*   //  buf[x]=90; */
+/*   for(y=-d;y<=d;y++) */
+/*     for(x=-d;x<=d;x++){ */
+/*       int xx=x+i,yy=y+j; */
+/*       if((0<=xx) && (xx<N) && (0<=yy) && (yy<N)) */
+/*       	buf[xx+N*yy]=4095; */
+/*     } */
+/*   return buf; */
+/* } */
+
+int all();
 
 int
 main()
 {
-
-  int i,j;
-  unsigned short*buf=malloc(NN*sizeof(*buf));
-  splat(5,0,12,buf);
+  return all();
+  /* int i,j; */
+  /* unsigned short*buf=malloc(NN*sizeof(*buf)); */
+  /* splat(5,0,12,buf); */
   
-  assert(0==mma_init());
+  /* assert(0==mma_init()); */
 
-  for(i=0;i<6;i+=4)
-    for(j=0;j<6;j+=4){
-      printf("%d %d\n",i,j);
-      splat(i,j,7,buf);
-      printf("bla\n");
-      if(0!=mma_upload_image(buf)){
-	printf("error upload-image\n");
-      }
-      printf("blub\n");
-    }
-  assert(0==mma_uninit());
-  return 0;
+  /* for(i=0;i<6;i+=4) */
+  /*   for(j=0;j<6;j+=4){ */
+  /*     printf("%d %d\n",i,j); */
+  /*     splat(i,j,7,buf); */
+  /*     printf("bla\n"); */
+  /*     if(0!=mma_upload_image(buf)){ */
+  /* 	printf("error upload-image\n"); */
+  /*     } */
+  /*     printf("blub\n"); */
+  /*   } */
+  /* assert(0==mma_uninit()); */
+  /* return 0; */
 }
 
